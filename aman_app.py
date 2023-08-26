91d8534540ebea7552ea0b8db2edc3c6b6d2f5ab
Am99473 <56793867+Am99473@users.noreply.github.com>
2023-08-27 02:50:49 +0530

    Update aman_app.py

--- aman_app.py
+++ aman_app.py
@@ -1,11 +1,23 @@
 import os
 from selenium import webdriver
+import os
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = str(os.environ.get("GOOGLE_CHROME_BIN"))
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(os.environ.get("CHROMEDRIVER_PATH"))
+import streamlit as st
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-gpu")
+options.add_argument("--disable-features=NetworkService")
+options.add_argument("--window-size=1920x1080")
+options.add_argument("--disable-features=VizDisplayCompositor")
+
+st.write('MY NAME IS AMAN TIWARI')
+driver = webdriver.Chrome(options=options)
 driver.get("http://www.python.org")
-print(driver.title)
+st.write(driver.title)
